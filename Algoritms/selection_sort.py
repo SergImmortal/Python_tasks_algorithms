b890024c647c4f5e3cb89989fd30d303898acee5
Author Name <sergimmortal@gmail.com>
2018-10-16 11:34:26 +0300

    Add binary search and selection sort algoritms

---
+++ Algoritms/selection_sort.py
@@ -0,0 +1,19 @@
+print('Selection sort algoritm example')
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+print(selectionSort([5, 6, 3, 7, 1, 4, 2, 8]))
