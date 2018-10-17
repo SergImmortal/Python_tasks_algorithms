15bc1498cb60c82fa4071bd4ad58fc084c492a42
Author Name <sergimmortal@gmail.com>
2018-10-17 09:21:26 +0300

    test commit subscribe v4

--- Algorithms/binaary_serch.py
+++ Algorithms/binaary_serch.py
@@ -17,7 +17,7 @@ def binary_search(target_list, item):
             low = mid+1
     return None
 
-my_list = [1, 3, 4, 5, 9, 12, 15, 17, 22, 42, 25, 26, 27]
+my_list = [1, 3, 4, 5, 9, 12, 15, 17, 22, 42, 25, 26, 27, 28]
 
 print(binary_search(my_list, 3))
 print(binary_search(my_list, 1))
